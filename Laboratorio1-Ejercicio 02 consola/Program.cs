4db448bd6fb9bdd24beb4312654602cdbdcabd47
CRISTHIAN <CRISTHIAN@DESKTOP-LS2ABPT>
2023-08-26 18:26:10 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio1-Ejercicio 02 consola/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+//Realiza un programa que reciba una cantidad de minutos y muestre
+//por pantalla a cuantashoras y minutos corresponde.
+//Por ejemplo: 1000 minutos son 16 horas y 40 minutos.
+
+Console.WriteLine("Cantidad de minutos");
+int min = int.Parse(Console.ReadLine());
+
+if (min%60==0)
+{
+    int horas = min/60;
+    Console.WriteLine("Son "+horas +" horas");
+}
+else
+{
+    int horas = min / 60;
+    int minutos = min%60;
+    Console.WriteLine("Son " + horas + " horas y "+minutos+" minutos");
+}
